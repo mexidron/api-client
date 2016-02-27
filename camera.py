601f11194ea4f9cf7b014dcbb21dc38fb4006fd0
Ruben Gonzalez <rubenrua@gmail.com>
2016-02-27 18:46:50 +0100

    Add camera module

---
+++ camera.py
@@ -0,0 +1,25 @@
+from gi.repository import Gst
+Gst.init(None)
+
+v4l2_device = "/dev/video1"
+pipeline_str = "v4l2src device={device} num-buffers=1 ! jpegenc ! filesink location={{img}}".format(device=v4l2_device)
+
+def take_pic(file_path):
+
+    # Build the pipeline
+    pipeline = Gst.parse_launch(pipeline_str.format(img=file_path))
+
+    # Start playing
+    pipeline.set_state(Gst.State.PLAYING)
+
+    # Wait until error or EOS
+    bus = pipeline.get_bus()
+    msg = bus.timed_pop_filtered(
+        Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+
+    # Free resources
+    pipeline.set_state(Gst.State.NULL)
+
+
+if __name__ == "__main__":
+    take_pic("test.jpeg")
