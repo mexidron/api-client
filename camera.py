f8fc59cabcb18d0cfcdc2fd664dcc18ab9421d3e
Ruben Gonzalez <rubenrua@gmail.com>
2016-02-27 19:46:34 +0100

    Fix camera.py for mac

--- camera.py
+++ camera.py
@@ -1,24 +1,35 @@
-from gi.repository import Gst
-Gst.init(None)
+import os
+import shutil
 
-v4l2_device = "/dev/video1"
-pipeline_str = "v4l2src device={device} num-buffers=1 ! jpegenc ! filesink location={{img}}".format(device=v4l2_device)
+try:
+    from gi.repository import Gst
+    Gst.init(None)
 
-def take_pic(file_path):
+    v4l2_device = "/dev/video1"
+    pipeline_str = "v4l2src device={device} num-buffers=1 ! jpegenc ! filesink location={{img}}".format(device=v4l2_device)
 
-    # Build the pipeline
-    pipeline = Gst.parse_launch(pipeline_str.format(img=file_path))
+    def take_pic(file_path):
 
-    # Start playing
-    pipeline.set_state(Gst.State.PLAYING)
+        # Build the pipeline
+        pipeline = Gst.parse_launch(pipeline_str.format(img=file_path))
 
-    # Wait until error or EOS
-    bus = pipeline.get_bus()
-    msg = bus.timed_pop_filtered(
-        Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+        # Start playing
+        pipeline.set_state(Gst.State.PLAYING)
 
-    # Free resources
-    pipeline.set_state(Gst.State.NULL)
+        # Wait until error or EOS
+        bus = pipeline.get_bus()
+        msg = bus.timed_pop_filtered(
+            Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+
+        # Free resources
+        pipeline.set_state(Gst.State.NULL)
+    
+except ImportError:
+    
+    def take_pic(file_path):
+        fake = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'file.jpeg')
+        shutil.copyfile(fake , file_path)
+
 
 
 if __name__ == "__main__":
