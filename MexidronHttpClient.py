9ed018a251e9bb818bf5fffd8378388163c21a11
pnieto <pnieto@teltek.es>
2016-02-27 17:07:21 +0100

    test upload to http server

---
+++ MexidronHttpClient.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+__version__ = "0.1"
+__all__ = ["MexidronHttpClient"]
+__author__ = "pnietoiglesias"
+__home_page__ = "http://github.com/mexidron/api-client"
+
+import os
+import requests
+
+class MexidronHttpClient:
+    def __init__(self, server="localhost"):
+        self.server=server
+        self.session=requests.Session()
+        self.session.headers.update({'referer': self.server})
+
+    def doUpload(self, filename):
+        multiple_files = [('file', (filename, open(filename, 'rb'), 'text/plain'))]
+        url=self.server
+        return(self.session.post(url, files=multiple_files))
+
+
+    def test(self):
+        response = self.doUpload("README.md")
+        print(response.text)
+
+if __name__ == '__main__':
+    mu=MexidronHttpClient("http://192.168.3.101:8000")
+    mu.test()
